29be0c0f6f95df68d517df6789ec13979598c7eb
Nando Machado <icenando@gmail.com>
2023-12-10 16:13:25 +0000

    move header to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import styles from "./page.module.scss";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className={styles.header}>
+          <div>CARAVARVEL-AI</div>
+          <span>
+            <div>ABOUT</div>
+            <div>CONTACT</div>
+          </span>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-import styles from "./page.module.scss";
 import { MainSection } from "@/app/(mainSection)/[mainSection]";
 
 export default function Home() {
-  return (
-    <>
-      <header className={styles.header}>
-        <div>CARAVARVEL-AI</div>
-        <span>
-          <div>ABOUT</div>
-          <div>CONTACT</div>
-        </span>
-      </header>
-      <MainSection />
-    </>
-  );
+  return <MainSection />;
 }
