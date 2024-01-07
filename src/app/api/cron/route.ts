6422cb0633c4f0dd045779c45e6448482ec4b68f
Nando Machado <icenando@gmail.com>
2024-01-07 09:10:47 +0000

    update revalidatePaths

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -2,4 +2,7 @@ import { revalidatePath } from "next/cache";
 
 export async function GET() {
   revalidatePath("/", "layout");
+  revalidatePath("/archive", "page");
+  revalidatePath("/archive/[eventId]", "page");
+
 }
