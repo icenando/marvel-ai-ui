23c4912cad78a1af2c83ce05dc850c08a0e03845
Nando Machado <icenando@gmail.com>
2024-01-07 11:26:18 +0000

    set dynamicPaths to false

--- src/app/archive/[eventId]/page.tsx
+++ src/app/archive/[eventId]/page.tsx
@@ -4,8 +4,8 @@ import { HeroImage } from "@/components/heroImage";
 import { EventsResult } from "@/types/dynamoResponse";
 import { fetchAllEvents, fetchSingleEvent } from "@/api/db";
 
-// If set to true, will fetch on demand. Otherwise, return 404 if path has not been generated.
-export const dynamicParams = true;
+// If set to true, will fetch ungenerated path on demand. Otherwise, return 404.
+export const dynamicParams = false;
 
 // Create links for the [eventId] dynamic path for all used events in DB
 export const generateStaticParams = async () => {
