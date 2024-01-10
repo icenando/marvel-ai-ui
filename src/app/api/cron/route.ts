8a8583c73aecb4f4869ebed24505291bb950084a
Nando Machado <icenando@gmail.com>
2024-01-10 09:20:04 +0000

    ditto

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,8 +1,7 @@
 import { revalidatePath } from "next/cache";
 
 export async function GET() {
-  revalidatePath("/");
-  revalidatePath("/(mainSection)/[mainSection]");
   revalidatePath("/archive");
   revalidatePath("/archive/[eventId]");
+  revalidatePath("/", "layout");
 }
