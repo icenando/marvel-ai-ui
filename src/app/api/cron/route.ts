393e61fbc34df5ea3e4053df917c5cb718d8d9cc
Nando Machado <nando.carnielmachado@sainsburys.co.uk>
2024-01-12 16:41:22 +0000

    change revalidatePath

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -2,7 +2,7 @@ import { revalidatePath } from "next/cache";
 
 export async function GET() {
   revalidatePath("/archive/page");
-  revalidatePath("/archive/[eventId]/page");
+  revalidatePath("/archive/[eventId]", "layout");
   revalidatePath("/page");
 
   return Response.json({ info: "finished running revalidatePath" });
