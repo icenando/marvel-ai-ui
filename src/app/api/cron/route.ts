b37f096ab6b2b245d89d710991cb214ea82dc78e
Nando Machado <icenando@gmail.com>
2024-01-14 10:31:03 +0000

    revalidatePath

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,9 +1,9 @@
 import { revalidatePath } from "next/cache";
 
 export async function GET() {
-  revalidatePath("/archive/page");
-  revalidatePath("/archive/[eventId]", "layout");
-  revalidatePath("/page");
+  revalidatePath("/archive/[eventId]/");
+  revalidatePath("/archive");
+  revalidatePath("/");
 
   return Response.json({ info: "finished running revalidatePath" });
 }
