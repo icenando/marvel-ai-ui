b22268c6bedfc2b6af2a1c7d8dbfe5f8b0b0c91d
Nando Machado <icenando@gmail.com>
2024-01-20 11:33:21 +0000

    message when no comments

--- src/components/commentsSection.tsx
+++ src/components/commentsSection.tsx
@@ -9,24 +9,32 @@ export const CommentsSection = (): JSX.Element => {
   const { comments, addComment, editComment, deleteComment } = useComment();
 
   const CommentsList = () =>
-    comments.map(comment => {
-      return (
-        <div key={comment.commentId} className={styles.commentCard}>
-          <div className={styles.commentCard__username}>{comment.username}</div>
-          <div className={styles.commentCard__options}>
-            <div className={styles.commentCard__comment__edit}>edit</div>
-            <div
-              className={styles.commentCard__comment__delete}
-              id={comment.commentId}
-              onClick={event => deleteComment(event.currentTarget.id)}
-            >
-              delete
+    comments.length ? (
+      comments.map(comment => {
+        return (
+          <div key={comment.commentId} className={styles.commentCard}>
+            <div className={styles.commentCard__username}>
+              {comment.username}
             </div>
+            <div className={styles.commentCard__options}>
+              <div className={styles.commentCard__comment__edit}>edit</div>
+              <div
+                className={styles.commentCard__comment__delete}
+                id={comment.commentId}
+                onClick={event => deleteComment(event.currentTarget.id)}
+              >
+                delete
+              </div>
+            </div>
+            <div className={styles.commentCard__comment}>{comment.comment}</div>
           </div>
-          <div className={styles.commentCard__comment}>{comment.comment}</div>
-        </div>
-      );
-    });
+        );
+      })
+    ) : (
+      <div className={styles.commentCard}>
+        <div className={styles.commentCard__comment}>Leave a comment</div>
+      </div>
+    );
 
   // TODO: this will come from the session once implemented
   const [isLoggedIn, setLoggedIn] = useState(true);
@@ -56,15 +64,6 @@ export const CommentsSection = (): JSX.Element => {
         <ActionButton />
       </div>
       <CommentsList />
-      {/* <button
-        onClick={() =>
-          addComment({
-            commentId: uuid(),
-            username: "test",
-            comment: "No, it doesn't",
-          })
-        }
-      /> */}
     </div>
   );
 };
