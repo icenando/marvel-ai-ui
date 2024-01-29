3e68f7b98782646c3c5d48fcb99c1d9584f9fa26
Nando Machado <icenando@gmail.com>
2024-01-29 08:37:54 +0000

    delete button shows form status

--- src/components/deleteButton.tsx
+++ src/components/deleteButton.tsx
@@ -9,11 +9,14 @@ export const DeleteButton = () => {
   const buttonText = pending ? "deleting" : "delete";
 
   return (
-    <div className={styles.commentCard__comment__delete}>
-      <button type="submit" aria-disabled={pending} disabled={pending}>
-        {buttonText}
-      </button>
-    </div>
+    <button
+      type="submit"
+      aria-disabled={pending}
+      disabled={pending}
+      className={styles.commentCard__deleteBtn}
+    >
+      {buttonText}
+    </button>
   );
 };
 
