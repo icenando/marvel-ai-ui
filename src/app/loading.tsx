c0f31912c272b10d659a212071a3655f385e9d64
Nando Machado <icenando@gmail.com>
2024-02-01 23:52:42 +0000

    add loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import loadingStyle from "./loading.module.scss";
+
+const Loading: React.FC = () => {
+  return (
+    <div className={loadingStyle.spinner_container}>
+      <div className={loadingStyle.hourglass}>
+        <div className={loadingStyle.sand}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
