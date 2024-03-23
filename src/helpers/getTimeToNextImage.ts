354e0a63543e79a4533975f70e7649e266f849e5
Nando Machado <icenando@gmail.com>
2024-03-23 11:53:29 +0000

    handle undefined time

--- src/helpers/getTimeToNextImage.ts
+++ src/helpers/getTimeToNextImage.ts
@@ -1,23 +1,23 @@
-import { formatDistanceStrict, intervalToDuration } from "date-fns";
+import { intervalToDuration } from "date-fns";
 import { useEffect, useState } from "react";
 
 export const useGetTimeToNextImage = () => {
   let nextUpdate = new Date();
   nextUpdate.setHours(10, 1, 0);
-
-  if (new Date() > nextUpdate) {
+  if (nextUpdate.getTime() < Date.now()) {
     nextUpdate = new Date(nextUpdate.getTime() + 24 * 60 * 60 * 1000);
   }
 
   const [now, setNow] = useState(Date.now());
   useEffect(() => {
-    setInterval(() => setNow(Date.now()), 1000);
+    const intervalId = setInterval(() => setNow(Date.now()), 1000);
+    return () => clearInterval(intervalId);
   });
 
   const timetoNewImage = intervalToDuration({ start: now, end: nextUpdate });
-  const hours = timetoNewImage.hours?.toString().padStart(2, "0");
-  const minutes = timetoNewImage.minutes?.toString().padStart(2, "0");
-  const seconds = timetoNewImage.seconds?.toString().padStart(2, "0");
+  const hours = (timetoNewImage.hours ?? 0).toString().padStart(2, "0");
+  const minutes = (timetoNewImage.minutes ?? 0).toString().padStart(2, "0");
+  const seconds = (timetoNewImage.seconds ?? 0).toString().padStart(2, "0");
 
   return `${hours}:${minutes}:${seconds}`;
 };
