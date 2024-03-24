8732c0d14cfd01b7c11f57da576da6133052a798
Nando Machado <icenando@gmail.com>
2024-03-24 12:32:01 +0000

    refreshes page when timeToNewImage is zero

--- src/components/nextImageInTile.tsx
+++ src/components/nextImageInTile.tsx
@@ -2,9 +2,14 @@
 
 import { useGetTimeToNextImage } from "@/helpers/getTimeToNextImage";
 import styles from "../app/archive/archive.module.scss";
+import { useRouter } from "next/navigation";
 
 export const NextImageInTile = () => {
   const timeToNewImage = useGetTimeToNextImage();
+
+  const router = useRouter();
+  if (timeToNewImage === "00:00:00") router.refresh();
+
   return (
     <div className={styles.thumbs_section_coming_next}>
       {`New image in \n${timeToNewImage}`}
