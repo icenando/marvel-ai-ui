1dc2cc34b8145ac1d6075a3c55b3fc24152f9da1
Nando Machado <icenando@gmail.com>
2024-03-24 13:36:19 +0000

    add revalidatePath to zero timeToNextImage

--- src/components/nextImageInTile.tsx
+++ src/components/nextImageInTile.tsx
@@ -3,12 +3,16 @@
 import { useGetTimeToNextImage } from "@/helpers/getTimeToNextImage";
 import styles from "../app/archive/archive.module.scss";
 import { useRouter } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
-export const NextImageInTile = () => {
+export const NextImageInTile = async () => {
   const timeToNewImage = useGetTimeToNextImage();
 
   const router = useRouter();
-  if (timeToNewImage === "00:00:00") router.refresh();
+  if (timeToNewImage === "00:00:00") {
+    revalidatePath("/archive");
+    router.refresh();
+  }
 
   return (
     <div className={styles.thumbs_section_coming_next}>
