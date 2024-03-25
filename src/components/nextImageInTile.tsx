f7f3b19909687f95fed394df0d3625731b489184
Nando Machado <icenando@gmail.com>
2024-03-25 18:47:55 +0000

    makes fetch to revalidate

--- src/components/nextImageInTile.tsx
+++ src/components/nextImageInTile.tsx
@@ -4,15 +4,24 @@ import { useGetTimeToNextImage } from "@/helpers/getTimeToNextImage";
 import styles from "../app/archive/archive.module.scss";
 import { useRouter } from "next/navigation";
 import { revalidatePath } from "next/cache";
+import { useEffect } from "react";
 
 export const NextImageInTile = () => {
   const timeToNewImage = useGetTimeToNextImage();
 
   const router = useRouter();
-  if (timeToNewImage === "00:00:00") {
-    revalidatePath("/archive");
-    router.refresh();
-  }
+
+  useEffect(() => {
+    if (timeToNewImage.split(":")[2].endsWith("0")) {
+      const protocol = window.location.protocol;
+      const host = window.location.hostname;
+      const port = window.location.port ? `:${window.location.port}` : "";
+      const baseUrl = `${protocol}//${host}${port}`;
+      const endpoint = `${baseUrl}/api/revalidate`;
+
+      fetch(endpoint).then(() => router.refresh());
+    }
+  }, [router, timeToNewImage]);
 
   return (
     <div className={styles.thumbs_section_coming_next}>
