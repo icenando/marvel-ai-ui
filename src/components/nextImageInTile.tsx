ae65fd6813b7ce914f3309db6a548c6e7bf550e9
Nando Machado <icenando@gmail.com>
2024-03-29 09:02:17 +0000

    trying to fix countdown

--- src/components/nextImageInTile.tsx
+++ src/components/nextImageInTile.tsx
@@ -3,29 +3,40 @@
 import { useGetTimeToNextImage } from "@/helpers/getTimeToNextImage";
 import styles from "../app/archive/archive.module.scss";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const NextImageInTile = () => {
-  const timeToNewImage = useGetTimeToNextImage();
-
   const router = useRouter();
+  const timeToNewImage = useGetTimeToNextImage();
+  const [countown, setCountdown] = useState(timeToNewImage);
 
   useEffect(() => {
-    if (timeToNewImage === "00:00:00") {
+    const refreshPage = async () => {
+      setCountdown("...generating");
       const protocol = window.location.protocol;
       const host = window.location.hostname;
       const port = window.location.port ? `:${window.location.port}` : "";
       const baseUrl = `${protocol}//${host}${port}`;
       const endpoint = `${baseUrl}/api/revalidate`;
-      fetch(endpoint);
 
-      router.refresh();
+      try {
+        await fetch(endpoint);
+        router.refresh();
+      } catch (error) {
+        console.error("Failed to revalidate:", error);
+      }
+    };
+
+    setCountdown(timeToNewImage);
+
+    if (timeToNewImage === "00:00:00") {
+      refreshPage();
     }
-  }, [router, timeToNewImage]);
+  }, [timeToNewImage, router]);
 
   return (
     <div className={styles.thumbs_section_coming_next}>
-      {`New image in \n${timeToNewImage}`}
+      {`New image in \n${countown}`}
     </div>
   );
 };
